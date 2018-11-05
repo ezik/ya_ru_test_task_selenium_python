e7225afb420e39caf213b95c01ad761b4165c9aa
Elena <elena.kolyshkina@mcmakler.de>
2018-11-05 14:24:35 +0100

    updated requirements.txt

--- conftest.py
+++ conftest.py
@@ -2,23 +2,24 @@ import pytest
 from fixtures.application import Application
 
 fixture = None
-BASE_URL = "https://ya.ru/"
+base_url = "https://ya.ru/"
 
 
 @pytest.fixture
 def app(request):
     global fixture
     browser = request.config.getoption("--browser")
+
     if fixture is None or not fixture.is_valid():
         print("Fixture was created...")
         fixture = Application(browser=browser)
-        fixture.session.open_main_page(BASE_URL)
+        fixture.session.open_main_page(base_url)
     return fixture
 
 
 @pytest.fixture(scope="function", autouse=True)
 def before(app):
-    app.session.open_main_page(BASE_URL)
+    app.session.open_main_page(base_url)
 
 
 @pytest.fixture(scope="session", autouse=True)
